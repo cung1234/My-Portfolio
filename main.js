9cce2297deceed937b46fa945ad8767600389adb
cung1234 <cnbawi08@gmail.com>
2025-06-23 11:29:09 -0400

    needs work

---
+++ main.js
@@ -0,0 +1,4 @@
+let about = document.querySelector(".sr-only");
+let homeBtn = document.querySelector(".home-btn");
+let workBtn = document.querySelector(".works");
+let contactBtn = document.querySelector(".contact-me");
