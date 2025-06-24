5a3ddc82353eb84dacd05d43195c85c37b909944
cung1234 <cnbawi08@gmail.com>
2025-06-24 14:22:49 -0400

    Navbar

--- main.js
+++ main.js
@@ -2,3 +2,30 @@ let about = document.querySelector(".sr-only");
 let homeBtn = document.querySelector(".home-btn");
 let workBtn = document.querySelector(".works");
 let contactBtn = document.querySelector(".contact-me");
+const hamburger = document.querySelector(".nav_hamburger");
+const linksContainer = document.querySelector(".nav_menu");
+const links = document.querySelectorAll(".nav_menu_link");
+
+hamburger.addEventListener("click", () => {
+    linksContainer.classList.toggle("active");
+    hamburger.classList.toggle("active");
+});
+
+window.addEventListener("resize", () => {
+    if (window.matchMedia("(max-width: 550px)").matches) {
+        closeMenu();
+    }
+});
+
+if (window.matchMedia("(max-width: 550px").matches) {
+    closeMenu();
+}
+
+function closeMenu() {
+    links.forEach((link) => {
+        link.addEventListener("click", () => {
+            linksContainer.classList.remove("active");
+            hamburger.classList.remove("active");
+        });
+    }); 
+}
